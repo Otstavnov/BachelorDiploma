0bf840418c06a3c1aa944f0e4ba21ee5505c97fa
Otstavnov A.M <alexotst58@yandex.ru>
2023-05-17 12:17:39 +0300

    считывание из бд

--- app/src/main/java/com/example/app_test_user/fragments/TestingFragment.java
+++ app/src/main/java/com/example/app_test_user/fragments/TestingFragment.java
@@ -2,20 +2,28 @@ package com.example.app_test_user.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.app_test_user.Question;
 import com.example.app_test_user.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TestingFragment extends Fragment {
@@ -23,11 +31,11 @@ public class TestingFragment extends Fragment {
     private TextView txtView;
     private Button ans1, ans2, ans3, ans4;
     private Question q1, q2, q3, q4, q5, q6, q7, q8, q9, q10;
+    private Question tempQuestion = null;
     private List<Question> questionList;
-    Question questTemp;
+    private Question questTemp;
     public int iter = 0;
-
-    private String  correct_ans;
+    private String correct_ans;
     private String section;
     private int pointOPP, pointBasic, pointArray;
 
@@ -39,7 +47,7 @@ public class TestingFragment extends Fragment {
         super.onStart();
         initQuest();
         initView();
-        questTemp = q1;
+        questTemp = questionList.get(0);
         txtView.setText(questTemp.getText());
         ans1.setText(questTemp.getAnswer1());
         ans2.setText(questTemp.getAnswer2());
@@ -175,16 +183,36 @@ public class TestingFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_testing, container, false);
 
 
-
-
-
         return inflater.inflate(R.layout.fragment_testing, container, false);
     }
 
 
-
     public void initQuest() {
         questionList = new ArrayList<>();
+        String idSection = "ООП-";
+        String id_temp = "";
+        for (int i = 2; i <= 10; i++) {
+            id_temp = idSection + i;
+            FirebaseDatabase
+                    .getInstance("https://otstavnovdiploma-default-rtdb.europe-west1.firebasedatabase.app/")
+                    .getReference()
+                    .child("Questions")
+                    .child("ООП")
+                    .child(id_temp).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            tempQuestion = snapshot.getValue(Question.class);
+                            questionList.add(tempQuestion);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+        }
+
+
         q1 = new Question("ООП",
                 "ООП-1",
                 "Java - это ... язык программирования. Заполните пропуск",
@@ -193,103 +221,104 @@ public class TestingFragment extends Fragment {
                 "теоретический",
                 "Всё вышеперечисленное",
                 "объектно-ориентированный");
-
-        q2 = new Question("ООП",
-                "ООП-2",
-                "Java - это ... язык программирования. Заполните пропуск",
-                "объектно-ориентированный",
-                "функциональный",
-                "теоретический",
-                "Всё вышеперечисленное",
-                "объектно-ориентированный");
-
-        q3 = new Question("ООП",
-                "ООП-3",
-                "Java - это ... язык программирования. Заполните пропуск",
-                "объектно-ориентированный",
-                "функциональный",
-                "теоретический",
-                "Всё вышеперечисленное",
-                "объектно-ориентированный");
-
-        q4 = new Question("ООП",
-                "ООП-4",
-                "Java - это ... язык программирования. Заполните пропуск",
-                "объектно-ориентированный",
-                "функциональный",
-                "теоретический",
-                "Всё вышеперечисленное",
-                "объектно-ориентированный");
-
-        q5 = new Question("ООП",
-                "ООП-5",
-                "Java - это ... язык программирования. Заполните пропуск",
-                "объектно-ориентированный",
-                "функциональный",
-                "теоретический",
-                "Всё вышеперечисленное",
-                "объектно-ориентированный");
-
-        q6 = new Question("ООП",
-                "ООП-6",
-                "Java - это ... язык программирования. Заполните пропуск",
-                "объектно-ориентированный",
-                "функциональный",
-                "теоретический",
-                "Всё вышеперечисленное",
-                "объектно-ориентированный");
-
-        q7 = new Question("ООП",
-                "ООП-7",
-                "Java - это ... язык программирования. Заполните пропуск",
-                "объектно-ориентированный",
-                "функциональный",
-                "теоретический",
-                "Всё вышеперечисленное",
-                "объектно-ориентированный");
-        q8 = new Question("ООП",
-                "ООП-8",
-                "Java - это ... язык программирования. Заполните пропуск",
-                "объектно-ориентированный",
-                "функциональный",
-                "теоретический",
-                "Всё вышеперечисленное",
-                "объектно-ориентированный");
-
-        q9 = new Question("ООП",
-                "ООП-9",
-                "Java - это ... язык программирования. Заполните пропуск",
-                "объектно-ориентированный",
-                "функциональный",
-                "теоретический",
-                "Всё вышеперечисленное",
-                "объектно-ориентированный");
-
-        q10 = new Question("ООП",
-                "ООП-10",
-                "Что такое наследование в Java?",
-                "Возможность создавать новый класс на основе уже существующего",
-                "Возможность создавать новый объект на основе уже существующего",
-                "Возможность создавать новый интерфейс на основе уже существующего",
-                "Возможность создавать новый метод на основе уже существующего",
-                "Возможность создавать новый класс на основе уже существующего");
-
         questionList.add(q1);
-        questionList.add(q2);
-        questionList.add(q3);
-        questionList.add(q4);
-        questionList.add(q5);
-        questionList.add(q6);
-        questionList.add(q7);
-        questionList.add(q8);
-        questionList.add(q9);
-        questionList.add(q10);
+
+//        q2 = new Question("ООП",
+//                "ООП-2",
+//                "Java - это ... язык программирования. Заполните пропуск",
+//                "объектно-ориентированный",
+//                "функциональный",
+//                "теоретический",
+//                "Всё вышеперечисленное",
+//                "объектно-ориентированный");
+//
+//        q3 = new Question("ООП",
+//                "ООП-3",
+//                "Java - это ... язык программирования. Заполните пропуск",
+//                "объектно-ориентированный",
+//                "функциональный",
+//                "теоретический",
+//                "Всё вышеперечисленное",
+//                "объектно-ориентированный");
+//
+//        q4 = new Question("ООП",
+//                "ООП-4",
+//                "Java - это ... язык программирования. Заполните пропуск",
+//                "объектно-ориентированный",
+//                "функциональный",
+//                "теоретический",
+//                "Всё вышеперечисленное",
+//                "объектно-ориентированный");
+//
+//        q5 = new Question("ООП",
+//                "ООП-5",
+//                "Java - это ... язык программирования. Заполните пропуск",
+//                "объектно-ориентированный",
+//                "функциональный",
+//                "теоретический",
+//                "Всё вышеперечисленное",
+//                "объектно-ориентированный");
+//
+//        q6 = new Question("ООП",
+//                "ООП-6",
+//                "Java - это ... язык программирования. Заполните пропуск",
+//                "объектно-ориентированный",
+//                "функциональный",
+//                "теоретический",
+//                "Всё вышеперечисленное",
+//                "объектно-ориентированный");
+//
+//        q7 = new Question("ООП",
+//                "ООП-7",
+//                "Java - это ... язык программирования. Заполните пропуск",
+//                "объектно-ориентированный",
+//                "функциональный",
+//                "теоретический",
+//                "Всё вышеперечисленное",
+//                "объектно-ориентированный");
+//        q8 = new Question("ООП",
+//                "ООП-8",
+//                "Java - это ... язык программирования. Заполните пропуск",
+//                "объектно-ориентированный",
+//                "функциональный",
+//                "теоретический",
+//                "Всё вышеперечисленное",
+//                "объектно-ориентированный");
+//
+//        q9 = new Question("ООП",
+//                "ООП-9",
+//                "Java - это ... язык программирования. Заполните пропуск",
+//                "объектно-ориентированный",
+//                "функциональный",
+//                "теоретический",
+//                "Всё вышеперечисленное",
+//                "объектно-ориентированный");
+//
+//        q10 = new Question("ООП",
+//                "ООП-10",
+//                "Что такое наследование в Java?",
+//                "Возможность создавать новый класс на основе уже существующего",
+//                "Возможность создавать новый объект на основе уже существующего",
+//                "Возможность создавать новый интерфейс на основе уже существующего",
+//                "Возможность создавать новый метод на основе уже существующего",
+//                "Возможность создавать новый класс на основе уже существующего");
+//        questionList.add(q1);
+//        questionList.add(q2);
+//        questionList.add(q3);
+//        questionList.add(q4);
+//        questionList.add(q5);
+//        questionList.add(q6);
+//        questionList.add(q7);
+//        questionList.add(q8);
+//        questionList.add(q9);
+//        questionList.add(q10);
     }
-    public void initView(){
+
+    public void initView() {
         txtView = (TextView) getActivity().findViewById(R.id.txt_TestQuest);
         ans1 = (Button) getActivity().findViewById(R.id.btn_answer1);
         ans2 = (Button) getActivity().findViewById(R.id.btn_answer2);
         ans3 = (Button) getActivity().findViewById(R.id.btn_answer3);
-        ans4 =(Button) getActivity().findViewById(R.id.btn_answer4);
+        ans4 = (Button) getActivity().findViewById(R.id.btn_answer4);
     }
 }
